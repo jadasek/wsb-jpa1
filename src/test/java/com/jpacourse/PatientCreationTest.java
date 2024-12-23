d38226a1260cac6d2c2c08bd9191c41f3f59bbed
Tymoteusz G <tymoteusz2731@wp.pl>
2024-12-23 12:13:19 +0100

    usuwanie zbędnych importów

--- src/test/java/com/jpacourse/PatientCreationTest.java
+++ src/test/java/com/jpacourse/PatientCreationTest.java
@@ -1,6 +1,5 @@
 package com.jpacourse;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.http.MediaType;
